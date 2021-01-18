446aaac9ff29fc6533f47f5cb18170114ae9fd60
Zac Crumpton <zac@Zacs-MacBook-Pro.local>
2021-01-18 14:22:27 -0700

    album controls ui

--- src/api/AlbumPage.jsx
+++ src/api/AlbumPage.jsx
@@ -1,8 +1,12 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { useParams } from "react-router-dom";
 import useFetchByPlatform from "../hooks/useFetchByPlatform";
 import StreamingContext from "../contexts/streamingContext";
 import TailSpinLoader from "../components/ui/TailSpinLoader";
+import { FaPlay } from "react-icons/fa";
+import { BiShuffle } from "react-icons/bi";
+import { BsHeart, BsHeartFill } from "react-icons/bs";
+import { AiFillInfoCircle as Info } from "react-icons/ai";
 
 const endpoints = {
   spotify: "https://api.spotify.com/v1/albums",
@@ -14,6 +18,13 @@ const AlbumPage = () => {
   const { id } = useParams();
   const { platform } = useContext(StreamingContext);
   const { loading, data } = useFetchByPlatform(`${endpoints[platform]}/${id}`);
+  const [saved, setSaved] = useState(false);
+
+  const ToggleSave = () => {
+    console.log(saved);
+    saved ? setSaved(false) : setSaved(true);
+    console.log(saved);
+  };
 
   if (loading) {
     return <TailSpinLoader />;
@@ -37,6 +48,33 @@ const AlbumPage = () => {
             </h2>
             <h2 className="album-page-date">{release_date.split("-")[0]}</h2>
           </div>
+        </div>
+        <div className="album-nav">
+          <button className="album-nav-btn clickable">
+            <div className="btn-content">
+              <FaPlay color="#111b24" />
+              <div className="ml-10">Play</div>
+            </div>
+          </button>
+          <button className="album-nav-btn clickable">
+            <div className="btn-content">
+              <BiShuffle size={25} color="#111b24" />{" "}
+              <div className="ml-10">Shuffle</div>
+            </div>
+          </button>
+          <button
+            className="album-add-btn clickable"
+            onClick={() => ToggleSave()}
+          >
+            {saved ? (
+              <BsHeartFill size={30} color="#00d4ff" />
+            ) : (
+              <BsHeart size={30} />
+            )}
+          </button>
+          <button className="album-credits-btn clickable">
+            <Info color="adbdcc" size={35} />
+          </button>
         </div>
       </div>
     </React.Fragment>
