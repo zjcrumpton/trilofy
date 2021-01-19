4b64bfa9b969e8c85134bc3896cb968608efc32a
Zac Crumpton <zac@Zacs-MacBook-Pro.local>
2021-01-18 21:38:30 -0700

    implement play button on song hover

--- src/components/ui/TracklistItems.jsx
+++ src/components/ui/TracklistItems.jsx
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaPlay } from "react-icons/fa";
 
 const TracklistItems = ({ tracks }) => {
-  return tracks.map((track, i) => {
-    const { track_number, name, artists, duration_ms } = track;
-    let minutes = Math.floor(duration_ms / 60000);
-    let seconds = ((duration_ms % 60000) / 1000).toFixed(0);
-    let duration = minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
-    return (
-      <tr className="hover-row" key={`item-${i}`}>
-        <td className="gray-num">{track_number}</td>
-        <td className="clickable">{name}</td>
-        <td className="clickable artist-link">{artists[0].name}</td>
-        <td>{duration}</td>
-      </tr>
-    );
-  });
+  return tracks.map((track, i) => <TrackItem track={track} index={i} />);
+};
+
+const TrackItem = ({ track, index }) => {
+  const [hovering, setHovering] = useState(false);
+
+  const toggleHover = () => {
+    hovering ? setHovering(false) : setHovering(true);
+  };
+
+  const { track_number, name, artists, duration_ms } = track;
+  let minutes = Math.floor(duration_ms / 60000);
+  let seconds = ((duration_ms % 60000) / 1000).toFixed(0);
+  let duration = minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+
+  return (
+    <tr
+      className="hover-row"
+      key={`item-${index}`}
+      onMouseEnter={() => toggleHover()}
+      onMouseLeave={() => toggleHover()}
+    >
+      <td className="gray-num">
+        {hovering ? (
+          <FaPlay className="clickable vert-centered" color="white" size={13} />
+        ) : (
+          track_number
+        )}
+      </td>
+      <td className="clickable">{name}</td>
+      <td className="clickable artist-link">{artists[0].name}</td>
+      <td>{duration}</td>
+    </tr>
+  );
 };
 
 export default TracklistItems;
